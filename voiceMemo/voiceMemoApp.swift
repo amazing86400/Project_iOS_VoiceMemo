442ae6ce7961e51a911984f104530762a2076a09
kbshin <amazing1997@naver.com>
2024-04-24 11:04:18 +0900

    Initial Commit

---
+++ voiceMemo/voiceMemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  voiceMemoApp.swift
+//  voiceMemo
+//
+//  Created by KIBEOM SHIN on 4/24/24.
+//
+
+import SwiftUI
+
+@main
+struct voiceMemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
