b33a251b246a8878adc728db337ea36fe5671810
kbshin <amazing1997@naver.com>
2024-05-01 19:15:06 +0900

    온보딩 화면 작업

---
+++ voiceMemo/Core/Extension/Color+Extensions.swift
@@ -0,0 +1,23 @@
+//
+//  Color+Extensions.swift
+//  voiceMemo
+//
+//  Created by KIBEOM SHIN on 4/25/24.
+//
+
+import Foundation
+import SwiftUI
+
+//extension Color {
+//    static let customBackgroundGreen = Color("customblack")
+//    static let customblack = Color("customblack")
+//    static let customCoolGray = Color("customCoolGray")
+//    static let customGray0 = Color("customGray0")
+//    static let customGray1 = Color("customGray1")
+//    static let customGray2 = Color("customGray2")
+//    static let customGreen = Color("customGreen")
+//    static let customIconGray = Color("customIconGray")
+//    static let customOrange = Color("customOrange")
+//    static let customSky = Color("customSky")
+//    static let customWhite = Color("customWhite")
+//}
---
+++ voiceMemo/Model/Onboarding/OnboardingContent2.swift
@@ -0,0 +1,14 @@
+//
+//  OnboardingContent2.swift
+//  voiceMemo
+//
+//  Created by KIBEOM SHIN on 4/25/24.
+//
+
+import Foundation
+
+struct OnboardingContent2: Hashable {
+    let imageFileName: String
+    let title: String
+    let subTitle: String
+}
